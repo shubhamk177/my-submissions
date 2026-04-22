7f0ae2eb454e4891d2ce2d3ec25f16d8d49c4884
shubhamk177 <shubhamkashyap87@gmail.com>
2026-04-22 05:37:27 +0000

    feat: daily code 2026-04-22

---
+++ mini-projects/2026-04-22.js
@@ -0,0 +1,11 @@
+// 2026-04-22 | Mini-project: Simple event emitter
+class EventEmitter {
+  #events = {};
+  on(event, fn)  { (this.#events[event] ??= []).push(fn); return this; }
+  off(event, fn) { this.#events[event] = (this.#events[event]||[]).filter(f=>f!==fn); }
+  emit(event, ...args) { (this.#events[event]||[]).forEach(fn => fn(...args)); }
+}
+
+const ee = new EventEmitter();
+ee.on('greet', name => console.log(`Hello, ${name}!`));
+ee.emit('greet', 'World'); // Hello, World!
